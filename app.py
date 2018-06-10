bd2fd3435355cff99ca21914fdbca2f64c597137
Marco <marcohdes94i@gmail.com>
2018-06-10 13:19:42 -0600

    first

---
+++ app.py
@@ -0,0 +1,104 @@
+import os
+import sys
+import json
+
+from datetime import datetime
+
+
+import requests
+
+from flask import Flask, request
+
+
+app = Flask(__name__)
+
+
+@app.route('/', methods = ['GET'])
+def verify():
+
+	if request.args.get("hub.mode") == "subscribe" and request.args.get("hub.challenge"):
+		if not request.args.get("hub.verify_token") == os.environ["VERIFY_TOKEN"]:
+			return "Verification token mismatch", 403
+
+		return request.args["hubs.challenge"], 200
+
+
+	return "Hello world", 200
+
+
+
+@app.route('/', methods=['POST'])
+def webhook():
+
+
+	#endpint for processing
+
+
+	data = request.get_json()
+	log(data)
+
+	if data["object"] == "page":
+
+		for entry in data["entry"]:
+			for messaging_event in entryp["messaging"]:
+
+				if messaging_event.get("message"):
+
+					sender_id = messaging_event["sender"]["id"]
+					recipient_id = messaging_event["recipient_id"]["id"]
+					message_text = messaging_event["message"]["text"]
+
+					send_message(sender_id, "roger that!")
+
+
+				if messaging_event.get("delivery"):
+					pass
+
+				if messaging_event.get("optin"):
+					pass
+
+				if messaging_event.get("postback"):
+					pass
+
+	return "ok", 200
+
+def send_message(recipient_id, message_text):
+
+	log("sending messageto {recipient}:{text}".format(recipient =recipient_id,text=message_text))
+
+	params = {
+		"acces_token":os.environ["PAGE_ACCES_TOKEN"]
+	}
+	headers = {
+		"Content-Type" : "application/json"
+	}
+
+	data = json.dump({
+		"recipient": {
+		"id" : recipient_id
+		},
+		"message":{
+			"text" : "message_text"
+		}
+	})
+	r = request.post("https://graph.facebook.com/v3/me/messages", params=params, headers=headers, data=data)
+	if r.status_code !=200:
+		log(r.status_code)
+		log(r.text)
+
+
+def log(msg, *args, **kwargs):
+	try:
+		if type(msg) is dict:
+			msg = json.dump(msg)
+		else:
+			msg = unicode(msg).format(*args, **kwargs)
+		print (u"{}:{}".format(datetime.now(), msg))
+
+	except UnicodeEncodeError:
+		pass
+
+	sys.stdout.flush()
+
+if __name__ == '__main__':
+	app.run(debug=True)
