a03a75ddd8676bf9a6de37d7c83b2bad1238d229
Marco <marcohdes94i@gmail.com>
2018-06-10 16:12:20 -0600

    Sdsd

--- app.py
+++ app.py
@@ -94,7 +94,7 @@ def send_message(recipient_id, message_text):
 			"text" : message_text
 		}
 	})
-	r = requests.post("https://graph.facebook.com/v3.0/Navollelp/messages", params=params, headers=headers, data=data)
+	r = requests.post("https://graph.facebook.com/v3.0/me/messages", params=params, headers=headers, data=data)
 	
 	
 	if r.status_code !=200:
