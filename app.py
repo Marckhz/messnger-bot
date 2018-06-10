17d09ca31a02160c138ebaeb5cb24ff65b247892
Marco <marcohdes94i@gmail.com>
2018-06-10 16:44:09 -0600

    Sds

--- app.py
+++ app.py
@@ -95,7 +95,7 @@ def send_message(recipient_id, message_text):
 			"text" : message_text
 		}
 	})
-	r = requests.post("https://graph.facebook.com/v3.0/me/messages", params=params, headers=headers, data=data)
+	r = requests.post("https://graph.facebook.com/v3.0/266547783912435/messages", params=params, headers=headers, data=data)
 	
 	
 	if r.status_code !=200:
